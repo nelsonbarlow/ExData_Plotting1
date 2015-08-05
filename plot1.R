e971468dde1d98c8516585337ffd6f53cbeea9de
Nelson Barlow <barlow@live.com>
2015-08-05 21:46:32 +0100

    first commit; adding assignment files

---
+++ plot1.R
@@ -0,0 +1,35 @@
+
+
+
+# using basic bash commands like grep and wc -l, I was able to determine
+# where the data for the 2 dates were in the file. First line of 
+# date 2007-02-01 was 66638, so I use the skip command to start there,
+# and read until 2880 lines after that which is the last row of 2007-02-02
+df <- read.table("household_power_consumption.txt", 
+                 na.strings = "?", 
+                 sep = ";",
+                 skip = 66637, nrows = 2880, 
+                 col.names = c("Date", 
+                               "Time", 
+                               "Global_active_power",
+                               "Global_reactive_power",
+                               "Voltage",
+                               "Global_intensity",
+                               "Sub_metering_1",
+                               "Sub_metering_2",
+                               "Sub_metering_3"
+                               )
+                 )
+
+#initiating device
+png(filename = "plot1.png", width = 480, height = 480)
+
+#plotting histogram
+hist(df$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power"
+     )
+
+#closing device
+dev.off()
